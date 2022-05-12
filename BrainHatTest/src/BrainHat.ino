6fa425f5d0e6c9d12b5daf92c91a58e50d26a14b
MartinPek <martin_pek@web.de>
2022-05-12 15:58:38 +0200

    brainhat: added led test

--- BrainHatTest/src/BrainHat.ino
+++ BrainHatTest/src/BrainHat.ino
@@ -14,6 +14,7 @@
 #include "header_st.h"
 
 #include <stb_common.h>
+#include <stb_led.h>
 #include <avr/wdt.h>
 
 
@@ -21,16 +22,31 @@ STB STB;
 
 PCF8574 relay;
 
+Adafruit_NeoPixel LED_Strips[STRIPE_CNT];
+
+const long int red = LED_Strips[0].Color(255,0,0);
+const long int green = LED_Strips[0].Color(0,255,0);
+const long int blue = LED_Strips[0].Color(0,0,255);
+
 
 void setup() {
     STB.begin();
-    STB.dbgln("new Stuff");
+    STB.dbgln("v1");
     Serial.println("WDT endabled");
     wdt_enable(WDTO_8S);
+    if (STB_LED::ledInit(LED_Strips, ledCnts, ledPins, NEO_BRG)) {Serial.println("LED: OK!");} else {Serial.println("LED: FAILED!");}
 
     STB.i2cScanner();
 
     wdt_reset();
+
+    STB_LED::setAllStripsToClr(LED_Strips, green);
+    delay(2000);
+    STB_LED::setAllStripsToClr(LED_Strips, red);
+    delay(2000);
+    STB_LED::setAllStripsToClr(LED_Strips, blue);
+    delay(2000);
+    
     STB.printSetupEnd();
     Serial.setTimeout(100);
     Serial.begin(9600);
@@ -41,7 +57,7 @@ void setup() {
 //===LOOP==============================
 //====================================*/
 void loop() {
-    STB.defaultOled.println(String(Serial.available()));
+    // STB.defaultOled.println(String(Serial.available()));
     if (Serial.available() > 0) {
         STB.defaultOled.println("Receiving: ");
         STB.defaultOled.println(String(Serial.read()));
