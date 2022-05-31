af202e7cbb4c8ca5ac4e80c2e8ba9d11fc93bc88
Martin Pek <Martin.Pek@web.de>
2022-06-01 00:03:15 +0200

    modified brainhat to test the speed of the bus

--- BrainHatTest/src/BrainHat.ino
+++ BrainHatTest/src/BrainHat.ino
@@ -22,7 +22,7 @@
 
 
 STB STB;
-
+unsigned long timestamp = millis();
 int testVal = 0;
 
 void setup() {
@@ -45,20 +45,24 @@ void setup() {
 //===LOOP==============================
 //====================================*/
 void loop() {
-
+    
+    // this can be used to fwd the serial input for the master to an usb port on the RPI
     /*
     if (Serial.available()) {
         Serial.write(Serial.read());
     }
     */
 
-
-    if (STB.rs485Write("!Relay_" + String(testVal) + "_0_")) {
-        testVal++;
-        if (testVal >= 8) {
-            testVal = 0;
-        }
+    /*
+    for (int testVal = 0; testVal < 4; testVal++) {
+        STB.rs485AddToBuffer("Brain response" + String(testVal));
     }
+    STB.rs485SendBuffer();
+    */
+
+    STB.rs485AddToBuffer(String(int(millis()-timestamp)));
+    timestamp = millis();
+    STB.rs485SendBuffer();
 
     // wdt_reset();
 }
