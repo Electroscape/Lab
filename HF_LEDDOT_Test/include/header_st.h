287d9d3f236a0c40b2b9b83723a101a9851b973b
MartinPek <martin_pek@web.de>
2022-07-13 16:17:31 +0200

    added HF_LEDDOT_test

---
+++ HF_LEDDOT_Test/include/header_st.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define RFID_1_SS_PIN           8     /* Per Konvention ist dies RFID-Port 1                                */
+#define RFID_2_SS_PIN           7     /* Per Konvention ist dies RFID-Port 2                                */
+#define RFID_3_SS_PIN           4     /* Per Konvention ist dies RFID-Port 3                                */
+#define RFID_4_SS_PIN           2     /* Per Konvention ist dies RFID-Port 4                                */
+
+
