73b4a3875d2812f66ec694e0cee5c7b8c499cb75
MartinPek <martin_pek@web.de>
2022-08-04 18:13:19 +0200

    modified HF led test to run on 4 pwms no blinking

--- HF_LED_Test/src/hf_LED_test.ino
+++ HF_LED_Test/src/hf_LED_test.ino
@@ -30,13 +30,14 @@ using namespace stb_namespace;
 
 /*==OLED======================================*/
 SSD1306AsciiWire oled;
-unsigned long lastHeartbeat = millis();
 
 // LEDS
-CRGB leds12V[NUM_LEDS];
-CRGB leds5V[NUM_LEDS];
+CRGB leds0[NUM_LEDS];
+CRGB leds1[NUM_LEDS];
+CRGB leds2[15];
+CRGB leds3[15];
 
-struct CRGB * ledarray[] ={leds12V, leds5V};
+struct CRGB * ledarray[] ={leds0, leds1, leds2, leds3};
 
 static uint menuStage = 0;
 
@@ -47,30 +48,20 @@ static uint menuStage = 0;
 - Interval (blinking)
 - "Flackern"
 */
-#define STAGECOUNT 7
+#define STAGECOUNT 5
 // TODO: use enum here for each stage so we have a name?
 static const char *menutexts[STAGECOUNT] = {
-    "Led selection\n 1: 12V, 2: 5V,\n 3: Both\n",
+    "Led selection\n 1-4\n 5: all\n",
     "Brightness\n from 0 to 255\n",
     "Red\n",
     "Green\n",
     "Blue\n",
-    "Time on duration\n in 100ms\n",
-    "Time off duration\n in 100ms\n"};
+};
+
 static char inputRaw[8] = "\0";
 
 static int charpos = 0;
 static int inputs[STAGECOUNT] = {}; 
-
-// first is power on Period second is power off period
-static unsigned long  ledPeriods[2][2] = {{1,0},{1,0}};
-// 0 is on 1 is off
-static int ledStage[2] = {0, 0};
-// static int ledCLRS[2][3] = {{0,0,0}, {0,0,0}};
-static CRGB ledCLRS[2] = {CRGB::Pink, CRGB::Pink};
-static unsigned long ledTimer[2] = {millis(), millis()};
-
-// fill_solid();
 
 
 
@@ -78,7 +69,13 @@ static unsigned long ledTimer[2] = {millis(), millis()};
 Keypad_I2C MyKeypad( makeKeymap(KeypadKeys), KeypadRowPins, KeypadColPins, KEYPAD_ROWS, KEYPAD_COLS, KEYPAD_I2C_ADD, PCF8574);
 
 void setup() {
-    brainSerialInit();
+
+    Serial.begin(115200);
+
+    Serial.println("WTF");
+    delay(2000);
+    Wire.begin();
+    Wire.setClock(100000);
     Serial.println(title);
     Serial.println(version);
 
@@ -103,65 +100,29 @@ void setup() {
 
 void loop() {
     Keypad_Update();
-    ledFlicker();
 }
 
 void set_led_clrs(CRGB clr, int stripe_no) {
     // Serial.print("setLedClrs");
     fill_solid(ledarray[stripe_no], NUM_LEDS, clr);
     FastLED.show();
+    delay(20);
 };
 
 void ledInit() {
-    FastLED.addLeds<LED_TYPE_12V,PWM_4_PIN,BRG>(leds12V, NUM_LEDS);
-    FastLED.addLeds<LED_TYPE_5V,PWM_3_PIN,GRB>(leds5V, NUM_LEDS);
+
+    FastLED.addLeds<LED_TYPE_12V,PWM_4_PIN,BRG>(leds0, NUM_LEDS);
+    FastLED.addLeds<LED_TYPE_12V,PWM_3_PIN,BRG>(leds1, NUM_LEDS);
+    FastLED.addLeds<LED_TYPE_12V,PWM_2_PIN,BRG>(leds2, NUM_LEDS);
+    FastLED.addLeds<LED_TYPE_12V,PWM_1_PIN,BRG>(leds3, NUM_LEDS);
     FastLED.setBrightness(255);
 
     CRGB someclr = CRGB(255,255,255);
-    Serial.println(someclr.r);
-    Serial.println(someclr.b);
-    Serial.print(someclr.g);
 
-
-    fill_solid(ledarray[0], NUM_LEDS, someclr);
-    fill_solid(leds5V, NUM_LEDS, CRGB(255,255,255));
-    FastLED.show();
-
-    delay(500);
-    fill_solid(leds12V, NUM_LEDS, CRGB::Red);
-    // fill_solid(leds5V, NUM_LEDS, CRGB::Red);
-    FastLED.show();
-    delay(500);
-
-    set_led_clrs(CRGB::Green, 0);
-    FastLED.show();
-    delay(599);
-    // set_led_clrs(CRGB::Green, 1);
-
-    
-}
-
-void ledFlicker() {
-    
-    for(int i = 0; i<2; i++) {
-        if (ledPeriods[i][1] == 0 || ledPeriods[i][0] == 0) continue;
-        // Serial.print("flicker check for"); Serial.println(i);
-        if (millis() - ledTimer[i] > 100 * ledPeriods[i][ledStage[i]]) {
-            ledTimer[i] = millis();
-            
-            if (ledStage[i] > 0) {
-                ledStage[i] = 0;
-                set_led_clrs(ledCLRS[i], i);
-            } else {
-                set_led_clrs(CRGB::Black, i);
-                ledStage[i]++;
-            }
-        }
+    for (int i=0; i<4; i++) {
+        set_led_clrs(CRGB(0,0,0), i);
     }
-   
-    // ledPeriods
-    // ledStage
-    // ledTimer 
+    
 }
 
 
@@ -222,36 +183,15 @@ void applyInputs() {
     Serial.println(selectedClr.g);
     Serial.println(selectedClr.b);
 
-    // 5V LED
-    if (inputs[0] >= 2) {
-        Serial.println("setting 5V Leds");
-        // fill_solid(leds5V, NUM_LEDS/2, selectedClr);
-        // FastLED.show();
-        set_led_clrs(selectedClr, 1);
-        ledCLRS[1] = selectedClr;
-        ledPeriods[1][0] = int(inputs[5]);
-        ledPeriods[1][1] = int(inputs[6]);
-        Serial.print("power on period ");
-        Serial.println(ledPeriods[1][0]*100);
-        Serial.print("power off period ");
-        Serial.println(ledPeriods[1][1]);
-    }   
-    // 12V LED
-    if ( inputs[0] != 2) {
-        Serial.println("setting 12V Leds");
-  
-        // fill_solid(leds12V, NUM_LEDS, selectedClr);
-        // FastLED.show();
-        set_led_clrs(selectedClr, 0);
-        ledCLRS[0] = selectedClr;
-
-        ledPeriods[0][0] = int(inputs[5]);
-        ledPeriods[0][1] = int(inputs[6]);
-
-        Serial.print("power on period ");
-        Serial.println(ledPeriods[0][0]*100);
-        Serial.print("power off period ");
-        Serial.println(ledPeriods[0][1]);
+    if (inputs[0] == 0) {
+        return;
+    }
+    if (inputs[0] < 4) {
+        set_led_clrs(selectedClr, inputs[0]);
+    } else {
+        for (int i=0; i<4; i++) {
+            set_led_clrs(CRGB(0,0,0), i);
+        }
     }
     
 };
