70031f80b2ce09148d8758bb0a350def35a12d4a
Martin Pek <martin.pek@web.de>
2024-04-05 13:15:50 +0200

    MotherHat added v0.0.6 relay test

--- MotherHat/src/MotherHat.ino
+++ MotherHat/src/MotherHat.ino
@@ -19,11 +19,24 @@ STB_MOTHER_IO MotherIO;
 void setup() {
     Mother.begin();
     MotherIO.ioInit();
-    Mother.relayInit();
+    Mother.relayInit(relayPinArray, relayInitArray, REL_AMOUNT);
     Serial.println("WDT endabled");
 }
 
 
 void loop() {
     Mother.rs485PerformPoll();
+    delay(1000);
+            /*
+    Mother.relayWrite(REL_0_PIN, HIGH);
+    Mother.relayWrite(REL_1_PIN, HIGH);
+    Mother.relayWrite(REL_2_PIN, HIGH);
+    Mother.relayWrite(REL_3_PIN, HIGH);
+    Mother.relayWrite(REL_4_PIN, HIGH);
+    Mother.relayWrite(REL_5_PIN, HIGH);
+    Mother.relayWrite(REL_6_PIN, HIGH);
+    Mother.relayWrite(REL_7_PIN, HIGH);
+    */
+
+    delay(1000);
 }
