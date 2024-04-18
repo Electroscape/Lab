c34343c5059698bca6a6e11696bc98a6882a62ed
Martin Pek <martin.pek@web.de>
2024-04-18 14:27:43 +0200

    image_sort: small cosmetic changes

--- images_sort_service/Rename_Foto.py
+++ images_sort_service/Rename_Foto.py
@@ -3,16 +3,18 @@ import datetime
 import shutil
 import time
 
-file_list = os.listdir() #list of files in Dir 
+file_list = os.listdir()    # list of files in Dir
 
-Dir = os.getcwd() + "\KassenFotos"  #path to folder with Images (new folders will be created) (manually write path)
+Dir = os.getcwd() + "\KassenFotos"  # path to folder with Images (new folders will be created) (manually write path)
 
 run = True
+
 
 def create_and_rename_new_folder(new_folder_name):
     new_folder_path = os.path.join(Dir, new_folder_name)
     os.makedirs(new_folder_path)
     return new_folder_path
+
 
 def new_folder_check(new_folder_name):
     folder_path = os.path.join(Dir, new_folder_name)
@@ -21,34 +23,37 @@ def new_folder_check(new_folder_name):
     else:
         create_and_rename_new_folder(new_folder_name)
 
+
 def image_create_time(file_path):
     new_image_creation_time = os.path.getctime(file_path)
     new_image_creation_date_time = datetime.datetime.fromtimestamp(new_image_creation_time)
     return new_image_creation_date_time
-    
+
+
 def sort_images(file):
     file_path = os.path.join(Dir, file)
-    sourcedir_foto = Dir + "/" #adding slash to path
+    sourcedir_foto = Dir + "/"
     creation_time = image_create_time(file_path)
 
-    file_ext_split = os.path.splitext(file_path) #file ending
-    file_ending = file_ext_split[1] #second part of file name
+    file_ext_split = os.path.splitext(file_path)    # file ending
+    file_ending = file_ext_split[1]     # second part of file name
 
     new_image_name = "Foto--" + creation_time.strftime("%H_%M_%S--%d_%m_%Y") + file_ending
-    counter_number = 2 #Number to count the diffrent image Versions
+    counter_number = 2  # Number to count the diffrent image Versions
     file_creation_date_file_name = "Kassenfotos_" + creation_time.strftime("%d_%m_%Y")
 
-    new_folder_check(file_creation_date_file_name) #check if folder exists
-    actual_folder_path = new_folder_check(file_creation_date_file_name) #folder for images
+    new_folder_check(file_creation_date_file_name)  # check if folder exists
+    actual_folder_path = new_folder_check(file_creation_date_file_name)     # folder for images
     
     while new_image_name in os.listdir(actual_folder_path):
         new_image_name = "Foto--" + creation_time.strftime("%H_%M_%S--%d_%m_%Y") + "(" + str(counter_number) + ")" + file_ending
-        counter_number = counter_number + 1 #set count number higher
+        counter_number = counter_number + 1
     else: 
-        counter_number = 2 #reset count number
+        counter_number = 2  # reset count number
     new_dir_image = sourcedir_foto + file
     os.rename(new_dir_image, sourcedir_foto + new_image_name)
     shutil.move(sourcedir_foto + new_image_name, actual_folder_path)
+
 
 def start_sort_process():
     for file in os.listdir(Dir):
@@ -56,7 +61,8 @@ def start_sort_process():
         if not os.path.isdir(path_check):
             sort_images(file)
 
-while run == True:
+
+while run:
     start_sort_process()
     time.sleep(3)
     print("run")
